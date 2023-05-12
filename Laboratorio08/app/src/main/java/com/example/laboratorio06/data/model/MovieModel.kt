1fa32d7c504b3e50b47ea6dd9a32a9d39ec3abad
XavierMoran <00106121@uca.edu.sv>
2023-05-11 23:37:50 -0600

    Laboratorio08

---
+++ Laboratorio08/app/src/main/java/com/example/laboratorio06/data/model/MovieModel.kt
@@ -0,0 +1,11 @@
+package com.example.laboratorio06.data.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class MovieModel (
+    val name: String,
+    val category: String,
+    val description: String,
+    val qualification: String,
+)
---
+++ Laboratorio08/app/src/main/java/com/example/laboratorio06/repositories/MovieRepository.kt
@@ -0,0 +1,8 @@
+package com.example.laboratorio06.repositories
+
+import com.example.laboratorio06.data.model.MovieModel
+
+class MovieRepository (private  val movies: MutableList<MovieModel>){
+    fun getMovies ()= movies
+    fun setMovies (movie: MovieModel) = movies.add(movie)
+}
